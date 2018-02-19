e08e84261e14d684874e626aaafec213b507387d
Bernhard Haring <bernhard.haring@edu.fh-joanneum.at>
2018-02-19 20:58:48 +0100

    first commit

--- src/main/java/at/kuchel/model/User.java
+++ src/main/java/at/kuchel/model/User.java
@@ -1,14 +1,30 @@
 package at.kuchel.model;
 
 
-public class User  extends AbstractEntity<Long> {
+import org.hibernate.validator.constraints.NotBlank;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "USER")
+public class User extends AbstractEntity<Long> {
 
-
+    @NotBlank
+    @Column(name = "USERNAME")
+    private String username;
 
     @Override
     protected Long getId() {
         return null;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
