6875a5d3d23efdf6c2c308c9665e58e32a4a8a36
Bernhard Haring <bernhard.haring@edu.fh-joanneum.at>
2018-02-21 19:58:05 +0100

    first site with db access

--- src/main/java/at/kuchel/service/UserService.java
+++ src/main/java/at/kuchel/service/UserService.java
@@ -5,6 +5,8 @@ import at.kuchel.repostitory.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UserService {
 
@@ -17,6 +19,10 @@ public class UserService {
 
     public void addUser(User user){
         userRepository.save(user);
+    }
+
+    public List<User> getAllUsers(){
+        return userRepository.findAll();
     }
 
 }
