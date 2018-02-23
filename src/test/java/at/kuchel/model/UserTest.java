c2dd713ba66e9fcc8a3fc77ec66e5be55a08f42a
Weglchri <christopher.wegl@gmail.com>
2018-02-23 15:28:04 +0100

    added usermodel test

---
+++ src/test/java/at/kuchel/model/UserTest.java
@@ -0,0 +1,37 @@
+package at.kuchel.model;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.Set;
+
+public class UserTest {
+
+
+    private User user;
+    private Role role;
+
+    @Before
+    public void SetupUser() {
+
+        user = new User();
+        role = new Role();
+        Set<Role> roles = Collections.singleton(role);
+
+        user.setId((long) 1);
+        user.setUsername("Random User");
+        user.setPassword("***");
+        user.setBirthday(LocalDate.now());
+        user.setMailAddress("radnom.user@gmail.com");
+        user.setRoles(roles);
+    }
+
+    @Test
+    public void getUserData() {
+        Assert.assertEquals("Random User", user.getUsername());
+        Assert.assertEquals(true, user.getRoles().contains(role));
+    }
+}
