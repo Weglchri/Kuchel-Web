21d6c10378345be33a6ac671a36c4988cae6e680
Weglchri <christopher.wegl@gmail.com>
2018-03-06 19:10:54 +0100

    added user admin

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,6 +1,8 @@
 INSERT INTO User(username, password, birthday) values ('bernhard', '$2a$10$ZY8AZ8Nkskn7mMdg8AKAAuHxdeJQmqO3bn6lPn7x0KW/Yogt7dlVy', '1980-04-02');
 INSERT INTO User(username, password, birthday) values ('wegl', '$2a$10$ZY8AZ8Nkskn7mMdg8AKAAuHxdeJQmqO3bn6lPn7x0KW/Yogt7dlVy', '1969-01-02');
 INSERT INTO User(username, password, birthday) values ('jasi', '$2a$10$ZY8AZ8Nkskn7mMdg8AKAAuHxdeJQmqO3bn6lPn7x0KW/Yogt7dlVy', '1995-12-31');
+INSERT INTO User(username, password, birthday) values ('admin', '$2a$10$ZY8AZ8Nkskn7mMdg8AKAAuHxdeJQmqO3bn6lPn7x0KW/Yogt7dlVy', '1992-12-02');
+
 
 INSERT INTO Ingredient(name,description,status) VALUES ('Bananen','Frucht','NEW');
 INSERT INTO Ingredient(name,description) VALUES ('Feige','Frucht');
@@ -28,4 +30,6 @@ INSERT INTO USER_ROLE (USER_ID,ROLE_ID) VALUES (1,1);
 INSERT INTO USER_ROLE (USER_ID,ROLE_ID) VALUES (1,2);
 INSERT INTO USER_ROLE (USER_ID,ROLE_ID) VALUES (2,1);
 INSERT INTO USER_ROLE (USER_ID,ROLE_ID) VALUES (3,1);
+INSERT INTO USER_ROLE (USER_ID,ROLE_ID) VALUES (4,1);
+INSERT INTO USER_ROLE (USER_ID,ROLE_ID) VALUES (4,2);
 
