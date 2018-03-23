fd16b1decffd48bea4acf3dbce5c246c8f771264
Bernhard Haring <bernhard.haring@edu.fh-joanneum.at>
2018-03-23 20:35:23 +0100

    added logging

--- src/main/java/at/kuchel/service/rest/RecipeServiceApi.java
+++ src/main/java/at/kuchel/service/rest/RecipeServiceApi.java
@@ -5,14 +5,18 @@ import at.kuchel.api.RecipeDetailedResponse;
 import at.kuchel.mapper.RecipeMapper;
 import at.kuchel.model.Recipe;
 import at.kuchel.service.RecipeService;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+import static org.slf4j.LoggerFactory.getLogger;
+
 @Service
 public class RecipeServiceApi {
+    private static final Logger LOG = getLogger(RecipeServiceApi.class);
 
     @Autowired
     private RecipeService recipeService;
@@ -21,16 +25,19 @@ public class RecipeServiceApi {
     private RecipeMapper recipeMapper;
 
     public List<RecipeDetailedResponse> getAllRecipes() {
+        LOG.info("Retrieve all recipes");
         List<Recipe> recipes = recipeService.getAllRecipes();
         return recipes.stream().map(recipe -> recipeMapper.mapToDetail(recipe)).collect(Collectors.toList());
     }
 
     public RecipeDetailedResponse getRecipeById(String id) {
+        LOG.info("Retrieve recipe with id '{}'", id);
         Recipe recipe = recipeService.getRecipeById(Long.parseLong(id));
         return recipeMapper.mapToDetail(recipe);
     }
 
     public List<RecipeDetailedResponse> getAllRecipes(LastSyncDateRequest lastSyncDateRequest) {
+        LOG.info("Retrieve recipe with sync date '{}'", lastSyncDateRequest.getLastSyncDate());
         List<Recipe> recipes = recipeService.getAllRecipes();
         return recipes.stream()
                 .filter(recipe -> recipe.getModifiedDate().after(lastSyncDateRequest.getLastSyncDate()))
